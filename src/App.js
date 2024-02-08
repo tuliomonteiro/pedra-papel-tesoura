d64af92f4b68d8c410a2e40be94f4d776466786a
Tulio Monteiro da Silva <tuliomonteiro@Tulios-MacBook-Pro-2.local>
2024-02-07 22:28:15 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <p>Testando</p>
+      <Game />
     </div>
   );
 }
 
 export default App;
 
-function Pedra() {}
+function Score() {}
 
-function Papel() {}
+function Game() {
+  const [userOption, setUserOption] = useState("");
 
-function Tesoura() {}
+  function setOption(e) {
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <button value="pedra" onClick={setOption}>
+        Pedra
+      </button>
+      <button value="papel" onClick={setOption}>
+        Papel
+      </button>
+      <button value="tesoura" onClick={setOption}>
+        Tesoura
+      </button>
+    </div>
+  );
+}
