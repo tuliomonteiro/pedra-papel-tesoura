0c39cb9ebac9e4b5637e6a7206c77e6a63535051
Tulio Monteiro da Silva <tuliomonteiro@Tulios-MacBook-Pro-2.local>
2024-02-08 22:03:05 -0300

    Adding some logic

--- src/App.js
+++ src/App.js
@@ -19,6 +19,30 @@ function Game() {
 
   function setOption(e) {
     console.log(e.target.value);
+    setUserOption(e.target.value);
+  }
+
+  function onResultHandle(e) {
+    const possibilities = ["pedra", "papel", "tesoura"];
+    let shuffled = possibilities
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    // console.log(shuffled);
+    if (shuffled[0] === userOption) {
+      console.log(
+        `Ocorreu um empate o computador escolheu${shuffled[0]} e o usuario ${userOption}`
+      );
+    } else if (shuffled[0] === "tesoura" && userOption === "papel") {
+      console.log(
+        `o computador escolheu${shuffled[0]} e o usuario escolheu ${userOption}, portanto o computador ganhou`
+      );
+    } else if (shuffled[0] === "papel" && userOption === "tesoura") {
+      console.log(
+        `O usuario ganhou pois escolheu ${userOption} e o computador ${shuffled[0]}`
+      );
+    }
   }
 
   return (
@@ -32,6 +56,7 @@ function Game() {
       <button value="tesoura" onClick={setOption}>
         Tesoura
       </button>
+      <button onClick={onResultHandle}>Resultado</button>
     </div>
   );
 }
